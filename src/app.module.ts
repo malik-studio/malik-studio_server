dc05f34d7837bf56fd0ba3f8ae982e3ce908a237
permalik <permalik@protonmail.com>
2024-06-23 21:11:59 -0500

    refactor
    abstract CustomConfigModule
    leverage env in TypeormModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,27 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { EmailsModule } from "./emails/emails.module";
-import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CustomConfigModule } from "./config/config.module";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: [".dev.env", ".prod.env", ".env"],
+    TypeOrmModule.forRootAsync({
+      imports: [CustomConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DEV_PG_MALIK_STUDIO_HOST"),
+        port: +configService.get<string>("DEV_PG_MALIK_STUDIO_PORT"),
+        username: configService.get<string>("DEV_PG_MALIK_STUDIO_USERNAME"),
+        password: configService.get<string>("DEV_PG_MALIK_STUDIO_PASSWORD"),
+        database: configService.get<string>("DEV_PG_MALIK_STUDIO_DATABASE"),
+        entities: [],
+        synchronize: configService.get<boolean>(
+          "DEV_PG_MALIK_STUDIO_SYNCHRONIZE",
+        ),
+      }),
+      inject: [ConfigService],
     }),
     EmailsModule,
   ],
